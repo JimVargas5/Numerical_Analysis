4d4d90555acfc70f78135ab95767292340d7c8ca
Jim Vargas <jimball1@hotmail.com>
2020-02-03 13:02:32 -0800

    Resolved all problems and now everything is dandy.

--- Numerical_Diff_Eqs/HW/HW2/5_4_5b.py
+++ Numerical_Diff_Eqs/HW/HW2/5_4_5b.py
@@ -1,6 +1,12 @@
 # Jim Vargas
 # 5.4.5b
 
+""" This code tests a third order Runge-Kutta method on the IVP
+    y'=lambda(y-cos(t))-sin(t), y(0)=1.
+The solution is y(t)=cos(t). It outputs convergence history to 
+a .txt file and graphs to a .pdf file. 
+"""
+
 import math
 from tabulate import tabulate
 from matplotlib import pyplot as plt
@@ -20,17 +26,19 @@ def nextStep(t,y,h,i):
     K1=f(t,y,i)
     K2=f(t + (1/2)*h, y + h*(1/2)*K1,i)
     K3=f(t + h, y + h*(-1*K1 + 2*K2),i)
-    return y + h*((1/6)*K1 + (2/6)*K2 + (1/6)*K3)
+    return y + h*((1/6)*K1 + (2/3)*K2 + (1/6)*K3)
 
-bigTable=[] # 0 to 2
+bigTable=[] # Indeces 0 to 2
 bigTable.append(['h', 'E_max','Ratio'])
 
 
-for i in range(2):
+
+for i in range(2): # Both lambdas
     bigTable.append(['-----','lambda: '+str(Lambda[i]),'-----'])
     for n in range(2,11+1):
         running_yk=[y0]
         running_Actual=[actualFunction(t0)]
+        running_Errors=[actualFunction(t0)-y0]
         running_AbsoluteErrors=[abs(actualFunction(t0)-y0)]
         running_t=[t0]
 
@@ -38,11 +46,11 @@ for i in range(2):
 
         y=y0
         t=t0
-        currentMaxVector=[0 for k in range(2+1)]
+        currentMaxVector=[0 for k in range(2+1)] # Represents a row in bigTable
         currentMaxVector[0]="2^(-"+str(n)+")"
         while t<=math.pi:
             y_next=nextStep(t,y,h,i)
-            temp=abs(actualFunction(t)-y_next)
+            temp=abs(actualFunction(t)-y)
             if temp > currentMaxVector[1]:
                 currentMaxVector[1]=temp
 
@@ -62,17 +70,18 @@ for i in range(2):
         plt.legend(loc='best')
 
         plt.subplot(1,2,2)
-        plt.plot(running_t,running_AbsoluteErrors)
+        plt.plot(running_t,running_AbsoluteErrors, label='Absolute Error')
         plt.title("Absolute Errors")
         plt.close()
         pdf.savefig(fig)
 
         bigTable.append(currentMaxVector)
 
-# bigTable rows 0,1,1+(11-2) are text
+# bigTable rows [0,1,1+(11-1)] are text
+# Ratio: E_max(h)/E_max(h/2)
 for row in range(3,len(bigTable)):
-    if row !=12 and row !=13: # skip first ratio
-        bigTable[row][2]=bigTable[row][1] / bigTable[row-1][1]
+    if row !=12 and row !=13: # Skip rows 2 and 13 as well
+        bigTable[row][2]=(bigTable[row][1] / bigTable[row-1][1]) ** (-1)
 
 
 with open("Output_5b.txt", "w") as text_file:
